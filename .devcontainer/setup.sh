b8b8a508e1791ca66d59f126fb79e759c1060974
Casimir Compaore <casiseba@hotmail.com>
2025-07-25 08:02:19 +0000

    deleted setup.sh in devcontainer as it is not used

--- .devcontainer/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set your Azure DevOps variables
-AZURE_REPO_URL="https://iaeabss.visualstudio.com/DefaultCollection/PBGL%20Portfolio/_git/qrcode"
-
-# Clone the Azure DevOps repo (if not already cloned)
-if [ ! -d "qrcode" ]; then
-  git clone $AZURE_REPO_URL qrcode
-fi
